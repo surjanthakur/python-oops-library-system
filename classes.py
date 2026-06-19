325dd6ac86ed7aec31a91af48733dac1c996f01c
surjanthakur <tsurjan506@gmail.com>
2026-06-20 01:43:04 +0530

    s

--- classes.py
+++ classes.py
@@ -1,6 +1,18 @@
+from uuid import uuid4
+
+
+class Book:
+    def __init__(self):
+        self.__book_id = str(uuid4())
+        self.__title: str
+        self.__author: str
+        self.__category: str
+        self.__is_available: bool
+
+
 class Library:
     def __init__(self):
-        self.library_books: list = []
+        self.library_books: list[object] = []
         self.borrowed_book_users: list[object] = []
 
     def add_book():
@@ -19,29 +31,11 @@ class Library:
         pass
 
 
-class Book:
-    def __init__(self):
-        self.__book_id: str
-        self.title: str
-        self.author: str
-        self.category: str
-        self.__is_available: bool
-
-    def borrow():
-        pass
-
-    def return_book():
-        pass
-
-    def get_status():
-        pass
-
-
 class User:
-    def __init__(self):
-        self.user_id: str
-        self.name: str
-        self.borrowed_books = []
+    def __init__(self, name: str):
+        self.__user_id = str(uuid4())
+        self.__name = name
+        self.__borrowed_books = []
 
     def borrow_book():
         pass
@@ -49,14 +43,18 @@ class User:
     def return_book():
         pass
 
+    def show_my_books(self):
+        if self.__borrowed_books:
+            return self.__borrowed_books
+        else:
+            return "oops! you shelf is empty📚"
 
-class Transaction:
-    def __init__(self):
-        self.book: str
-        self.user: str
-        self.issue_date: str
-        self.return_date: str
-        self.fine_calculate: str
 
-    def print_receipt():
-        pass
+class Student(User):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+
+class Teacher(User):
+    def __init__(self, name: str):
+        super().__init__(name)
