2cac1a2e83cb11b0906b0292dec58a5c5a596b2c
surjanthakur <tsurjan506@gmail.com>
2026-06-19 21:48:16 +0530

    s

--- library.py
+++ library.py
@@ -1,4 +1,19 @@
 class Library:
     def __init__(self):
         library_books: list = []
-        borrowed_books_users: list[object] = []
+        borrowed_book_users: list[object] = []
+
+    def add_book():
+        pass
+
+    def register_book():
+        pass
+
+    def issue_book():
+        pass
+
+    def accept_return():
+        pass
+
+    def show_available_books():
+        pass
